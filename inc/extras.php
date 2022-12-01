4792e378fc106909a973bb6251b7de35c0e4e332
rcastillod <rodrigo.castillo@global3.cl>
2022-11-30 22:03:07 -0300

    Feat: Featured home product responsive css

--- inc/extras.php
+++ inc/extras.php
@@ -2,3 +2,13 @@
 
 // Add new image size
 add_image_size('home-featured-product', 490, 690); // Featured home product
+
+// Disable dashicons
+add_action('wp_enqueue_scripts', 'aiooc_crunchify_dequeue_dashicon');
+function aiooc_crunchify_dequeue_dashicon()
+{
+  if (current_user_can('update_core')) {
+    return;
+  }
+  wp_deregister_style('dashicons');
+}
