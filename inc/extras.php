15b329d517d154f8ab8ccbfeac08f68f2edb7255
rcastillod <rodrigo.castillo@global3.cl>
2022-12-08 21:12:58 -0300

    refactor: :fire: Remove featured product image size

--- inc/extras.php
+++ inc/extras.php
@@ -1,8 +1,5 @@
 <?php
 
-// Add new image size
-add_image_size('home-featured-product', 490, 690); // Featured home product
-
 // Disable dashicons
 add_action('wp_enqueue_scripts', 'aiooc_crunchify_dequeue_dashicon');
 function aiooc_crunchify_dequeue_dashicon()
