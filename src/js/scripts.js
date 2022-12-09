7f3f36c2ea1ff0536a72f0d966c8fccbef547561
rcastillod <rodrigo.castillo@global3.cl>
2022-12-09 09:40:52 -0300

    feat: Activate autoplay and add pagination color slider

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,13 +1,12 @@
 /* ------------------------------- Hero SLider ------------------------------ */
 let swiper = new Swiper("#heroSlider", {
-  loop: false,
+  loop: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
-  autoplay: false,
-  // autoplay: {
-  //   delay: 8000,
-  //   disableOnInteraction: true,
-  // },
+  autoplay: {
+    delay: 8000,
+    disableOnInteraction: true,
+  },
 });
