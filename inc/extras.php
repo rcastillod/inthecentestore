b9d2df5cf854bcc30c051290d27eb2da491fdea0
rcastillod <rodrigo.castillo@global3.cl>
2022-12-14 14:27:03 -0300

    feat: :sparkles: Show wpform after submission

--- inc/extras.php
+++ inc/extras.php
@@ -9,3 +9,26 @@ function aiooc_crunchify_dequeue_dashicon()
   }
   wp_deregister_style('dashicons');
 }
+
+// Show contact form after submit
+function wpf_dev_frontend_output_success($form_data, $fields, $entry_id)
+{
+
+  // Optional, you can limit to specific forms. Below, we restrict output to form #235.
+  if (absint($form_data['id']) !== 13) {
+    return;
+  }
+  // Reset the fields to blank
+  unset(
+    $_GET['wpforms_return'],
+    $_POST['wpforms']['id']
+  );
+
+  // Uncomment this line out if you want to clear the form field values after submission
+  unset($_POST['wpforms']['fields']);
+
+  // Actually render the form.
+  wpforms()->frontend->output($form_data['id']);
+}
+
+add_action('wpforms_frontend_output_success', 'wpf_dev_frontend_output_success', 10, 3);
