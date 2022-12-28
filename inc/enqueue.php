b146122603a2b61bc166c87b9027ae5a52f0a767
rcastillod <rodrigo.castillo@global3.cl>
2022-12-28 11:01:25 -0300

    perf: :zap: Enqueue swiper files locally

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -36,10 +36,10 @@ function child_enqueue_styles()
   /** ITC theme style */
   wp_enqueue_style('itc-css', get_stylesheet_directory_uri() . '/dist/css/style.css', array('astra-theme-css'), CHILD_THEME_ASTRA_CHILD_VERSION, 'all');
   /** Swiper style */
-  wp_enqueue_style('swiper-css', 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css', '', CHILD_THEME_ASTRA_CHILD_VERSION, 'all');
+  wp_enqueue_style('swiper-css', get_stylesheet_directory_uri() . '/dist/css/external/external.css', '', CHILD_THEME_ASTRA_CHILD_VERSION, 'all');
 
   /** Swiper scripts */
-  wp_enqueue_script('swiper-js', 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js', '', '8.4.4', true);
+  wp_enqueue_script('swiper-js', get_stylesheet_directory_uri() . '/dist/js/external/external.js', '', '8.4.4', true);
   /** ITC scripts */
   wp_enqueue_script('itc-scripts', get_stylesheet_directory_uri() . '/dist/js/scripts.js', '', '1.0.0', true);
 }
