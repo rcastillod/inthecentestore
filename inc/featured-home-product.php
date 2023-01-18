79c923531cf1a8f0e98b5cf8fe9b0424c91300bb
rcastillod <rodrigo.castillo@global3.cl>
2023-01-18 16:11:21 -0300

    fix: :bug: Fix home featured shortcode deprecated function

--- inc/featured-home-product.php
+++ inc/featured-home-product.php
@@ -10,25 +10,28 @@ function itc_featured_product_sht()
 
   $productId = get_field('featured_home_product', 'option');
   $product = wc_get_product($productId->ID);
-  $productUrl = get_permalink($productId->ID);
-  $cat_id = get_cat_ID($product->get_categories());
+  $image = get_the_post_thumbnail($productId->ID, 'home-featured-product');
+  $title = get_the_title($productId->ID);
+  $description = $product->get_description();
+  $permalink = get_permalink($productId->ID);
+  $cat_id = get_cat_ID(wc_get_product_category_list($productId->ID));
   $cat_url = get_category_link($cat_id);
 ?>
 
   <div class="featured-product flex flex-col md:flex-row md:items-center lg:gap-8 xl:gap-20 pb-12 md:pb-0 pl-10 md:pl-0">
     <div class="featured-product__image -mt-14 mr-14 mb-14">
-      <a href="<?php echo $productUrl; ?>">
-        <?php echo $product->get_image('home-featured-product'); ?>
+      <a href="<?php echo $permalink; ?>">
+        <?php echo $image; ?>
       </a>
     </div>
     <div class="featured-product__content">
-      <h6 class="featured-product__category"><a href="<?php echo $cat_url; ?>"><?php echo $product->get_categories(); ?></a></h6>
-      <h2 class="featured-product__title"><?php echo $product->get_name(); ?></h2>
+      <h6 class="featured-product__category"><a href="<?php echo $cat_url; ?>"><?php echo wc_get_product_category_list($productId->ID); ?></a></h6>
+      <h2 class="featured-product__title"><?php echo $title; ?></h2>
       <div class="featured-product__description mb-10">
-        <?php echo $product->get_description(); ?>
+        <?php echo $description; ?>
       </div>
       <div class="elementor-button-wrapper">
-        <a href="<?php echo $productUrl; ?>" class="elementor-button-link elementor-button elementor-size-sm">Ver Más</a>
+        <a href="<?php echo $permalink; ?>" class="elementor-button-link elementor-button elementor-size-sm">Ver Más</a>
       </div>
     </div>
   </div>
