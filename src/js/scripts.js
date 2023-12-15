f34643004231dc3da85baf8ae88eba9121299c21
rcastillod <rodrigo.castillo@global3.cl>
2023-12-15 16:57:53 -0300

    fix: :bug: Fix the "k" problem in rut formater

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,59 +1,67 @@
 /* ------------------------------- Hero SLider ------------------------------ */
 let swiper = new Swiper("#heroSlider", {
-  lazy: true,
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  autoplay: {
-    delay: 8000,
-    disableOnInteraction: true,
-  },
+	lazy: true,
+	loop: true,
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+	autoplay: {
+		delay: 8000,
+		disableOnInteraction: true,
+	},
 });
 
 /* ---------------------- Validate rut field and format --------------------- */
 
 // Validate rut
 const validateRUT = (rut) => {
-  var regex = /^0*(\d{1,3}(\.?\d{3})*)\-?([\dkK])$/;
-  if (!regex.test(rut)) {
-    return false;
-  }
-  rut = rut.replace(/\./g, '').replace('-', '');
-  var dv = rut.slice(-1).toUpperCase();
-  var rutNum = parseInt(rut.slice(0, -1));
-  var M = 0, S = 1;
-  for (; rutNum; rutNum = Math.floor(rutNum / 10)) {
-    S = (S + rutNum % 10 * (9 - M++ % 6)) % 11;
-  }
-  return (dv == (S ? S - 1 + '' : 'K'));
+	var regex = /^0*(\d{1,3}(\.?\d{3})*)\-?([\dk])$/i; // Case-insensitive match for 'k'
+	if (!regex.test(rut)) {
+		return false;
+	}
+	rut = rut.replace(/\./g, '').replace('-', '');
+	var dv = rut.slice(-1).toUpperCase();
+	var rutNum = parseInt(rut.slice(0, -1));
+	var M = 0, S = 1;
+	for (; rutNum; rutNum = Math.floor(rutNum / 10)) {
+		S = (S + rutNum % 10 * (9 - M++ % 6)) % 11;
+	}
+	return (dv == (S ? S - 1 + '' : 'K'));
 }
 
 // Format rut
 const formatRUT = (rutField) => {
-  var rut = rutField.value.replace(/\D/g, '');
-  rut = rut.replace(/^0+/, '');
-  if (rut.length > 1) {
-    rut = rut.replace(/^(\d{1,8})(\d{1})$/, '$1-$2');
-  } else {
-    rut = rut.replace(/(\d)(?=(\d{3})+$)/g, '$1.');
-  }
-  rutField.value = rut;
+	var rut = rutField.value.replace(/[^\dkK]/g, ''); // Preserve 'K' in the regex
+	rut = rut.replace(/^0+/, '');
+	if (rut.length > 1) {
+		rut = rut.replace(/^(\d{1,8})(\d{1})$/, '$1-$2');
+	} else {
+		rut = rut.replace(/(\d)(?=(\d{3})+$)/g, '$1');
+	}
+	rutField.value = rut.toUpperCase(); // Convert to uppercase
+
+	// Add hyphen before 'K' if it's at the end
+	rutField.value = rutField.value.replace(/(\d+)([kK])$/, '$1-$2');
 }
+
+
+
+
+
 
 // Get the billing_rut field
 const rutField = document.getElementById('billing_rut');
 
 if (rutField) {
-  rutField.addEventListener('keyup', function () {
-    formatRUT(rutField);
-  });
-  rutField.addEventListener('focusout', function () {
-    if (!validateRUT(rutField.value) && !rutField.value == '') {
-      alert('El RUT no es valido');
-      rutField.value = ''
-      rutField.focus()
-    }
-  });
+	rutField.addEventListener('keyup', function () {
+		formatRUT(rutField);
+	});
+	rutField.addEventListener('focusout', function () {
+		if (!validateRUT(rutField.value) && !rutField.value == '') {
+			alert('El RUT no es valido');
+			rutField.value = ''
+			rutField.focus()
+		}
+	});
 }
